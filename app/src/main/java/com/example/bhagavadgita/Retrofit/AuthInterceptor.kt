d3fb956533f98ab45d240e5bde938a347ffe0310
AakashPatil1 <aakashap1818@gmail.com>
2024-06-15 11:59:48 +0530

    migrate

--- app/src/main/java/com/example/bhagavadgita/Retrofit/AuthInterceptor.kt
+++ app/src/main/java/com/example/bhagavadgita/Retrofit/AuthInterceptor.kt
@@ -1,22 +1,17 @@
-package com.example.bhagavadgita.Retrofit;
+package com.example.bhagavadgita.Retrofit
 
-import java.io.IOException;
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
 
-import okhttp3.Interceptor;
-import okhttp3.Request;
-import okhttp3.Response;
-
-public class AuthInterceptor implements Interceptor {
-
-    @Override
-    public Response intercept(Chain chain) throws IOException {
-        Request originalRequest = chain.request();
-
-        Request authenticatedRequest = originalRequest.newBuilder()
-                .addHeader("X-RapidAPI-Key", "Enter your api key")
-                .addHeader("X-RapidAPI-Host", "bhagavad-gita3.p.rapidapi.com")
-                .build();
-
-        return chain.proceed(authenticatedRequest);
+class AuthInterceptor : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val authenticatedRequest = originalRequest.newBuilder()
+            .addHeader("X-RapidAPI-Key", "Enter your api key")
+            .addHeader("X-RapidAPI-Host", "bhagavad-gita3.p.rapidapi.com")
+            .build()
+        return chain.proceed(authenticatedRequest)
     }
 }
